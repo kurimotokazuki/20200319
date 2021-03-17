4d2ee9a7fd35543a8a2617305e74207fc6a755f4
kazukikurimoto <kurik6548@gmail.com>
2021-03-17 21:25:18 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$people = 'Saburo';
+
+if ($people === 'Taro') {
+    echo '太郎です';
+} elseif ($people === 'Jiro') {
+    echo '次郎です';
+} elseif ($people === 'Saburo') {
+    echo '三郎です';
+} else {
+    echo 'それ以外です';
+}
